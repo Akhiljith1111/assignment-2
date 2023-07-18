70e89aceedd06bb6ff61a5a68a033232cb0da6fa
akhil8086 <akhiljith.r@edstem.com>
2023-07-18 16:50:23 +0530

    commit

--- Higher order functions/assign1.js
+++ Higher order functions/assign1.js
@@ -4,6 +4,7 @@ function square(num) {
   
   const numbers = [2, 3, 5, 7, 9];
   
-  numbers.forEach((num) => {
-    console.log(square(num));
+  numbers.forEach(function(num) {
+    
+   return console.log(square(num));
   });
