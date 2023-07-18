6f505851233c5e92524832d6589396771ce6d7bc
akhil8086 <akhiljith.r@edstem.com>
2023-07-18 14:04:12 +0530

    change in functions

--- Objects/assign5.js
+++ Objects/assign5.js
@@ -1,29 +1,26 @@
-function areAnagrams(word1, word2) {
-    if (word1.length !== word2.length) {
-      return false;
-    }
-  
-    const charCount1 = {};
-    const charCount2 = {};
-  
-    for (let char of word1) {
-      charCount1[char] = (charCount1[char] || 0) + 1;
-    }
-  
-    for (let char of word2) {
-      charCount2[char] = (charCount2[char] || 0) + 1;
-    }
-  
-    for (let char in charCount1) {
-      if (charCount1[char] !== charCount2[char]) {
-        return false;
-      }
-    }
-  
-    return true;
-  }
+let input1 = "happy"
+let input2 = "happy"
 
-  console.log(areAnagrams("listen", "silent"))
-  console.log(areAnagrams("hello", "holla"))
-  console.log(areAnagrams("good", "dog"))
-  
+function checkAnagrams(w0ord1, w0ord2){
+    let word1 = w0ord1.split("")
+    let word2 = w0ord2.split("")
+
+    if(word1.length !== word2.length) return False
+
+    let obj1 = word1.reduce((acc, curValue) => {
+        acc[curValue] = (acc[curValue] || 0) + 1;
+        return acc
+    }, {})
+    let obj2 = word2.reduce((acc, curValue) => {
+        acc[curValue] = (acc[curValue] || 0) + 1;
+        return acc
+    }, {})
+
+    for (let key1 in obj1) {
+        if(!word2.includes(key1)) return false
+        if(!obj1[key1]== obj2[key1]) return false
+        else return true
+    }       
+}
+
+console.log(checkAnagrams(input1,input2))
