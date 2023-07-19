9ffc6a3e3bf7e092362a9c03cca7cd4854e601ba
akhil8086 <akhiljith.r@edstem.com>
2023-07-19 13:24:05 +0530

    functions changed

--- Functions/assign1.js
+++ Functions/assign1.js
@@ -1,9 +1,9 @@
 
-function addNumbers(){
-    const num1 = 5
-    const num2 = 3
-    addNumbers = num1 + num2
-    console.log(addNumbers)
-}
+var num1 = 5
+var num2 = 5
 
+function addNumbers(){
+    addNumbers = num1 + num2
+}
 addNumbers()
+console.log(addNumbers)
