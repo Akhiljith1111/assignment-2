1714369713376eee241b023e144f684199f352a5
akhil8086 <akhiljith.r@edstem.com>
2023-07-19 11:36:22 +0530

    changes in 2d Arrays

--- Higher order functions/assign1.js
+++ Higher order functions/assign1.js
@@ -4,7 +4,6 @@ function square(num) {
   
   const numbers = [2, 3, 5, 7, 9];
   
-  numbers.forEach(function(num) {
-    
-   return console.log(square(num));
+  numbers.forEach(function (num) {
+    return console.log(square(num));
   });
