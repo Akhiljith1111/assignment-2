9ff64fce790b3373c087ce26c1a46a08a39d5163
akhil8086 <akhiljith.r@edstem.com>
2023-07-21 11:57:13 +0530

    empty

--- Higher order functions/assign4.js
+++ Higher order functions/assign4.js
@@ -1,5 +1,5 @@
 const inputArray = [2, 3, 4];
 
-const product = inputArray.reduce((accumulator, num) => accumulator * num, 1);
+const product = inputArray.reduce((accumulator, e) => accumulator * e);
 
 console.log(product)
